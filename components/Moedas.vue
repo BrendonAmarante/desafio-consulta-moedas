fffbc67116b0c7321ccdd8d07cab2073e5e31fb4
BrendonAmarante <brendon-amarante@live.com>
2022-04-21 17:36:47 -0300

    final-version

--- components/Moedas.vue
+++ components/Moedas.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="bg-indigo-200">
-        {{}}
+    {{carregarValores()}}
         <div class="grid grid-cols-1 lg:grid-cols-2">
         <div class="bg-blue-600  lg:h-screen lg:justify-center lg:min-h-screen lg:flex lg:items-center p-8 sm:p-12">
             <div class="flex-grow">
-                <h1 class="text-white text-center text-2xl sm:text-5xl mb-2 self-auto">Moedas</h1><br>
-                <p class="text-center text-lg text-blue-200">Pesquise uma moeda pela sua sigla!</p><br>
+                <h1 class="text-white text-center text-2xl sm:text-5xl mb-2 self-auto">Cryptomoeda</h1><br>
+                <p class="text-center text-lg text-blue-200">Pesquise uma Cryptomoeda pelo seu ticker!</p><br>
                 <div class="items-center flex ">
-                    <input class="flex-1 w-full mr-2 text-gray-700 px-4 py-1 bg-gray-300 rounded-md hover:bg-gray-200 focus:bg-white focus:outline-none border border-gray-300" type="text" placeholder="ID da Moeda" name="author" v-model="name">
+                    <input class="flex-1 w-full mr-2 text-gray-700 px-4 py-1 bg-gray-300 rounded-md hover:bg-gray-200 focus:bg-white focus:outline-none border border-gray-300" type="text" placeholder="Ticker da cryptomoeda! Ex: USDT/BTC" name="author" v-model="name">
                     <button class="flex-shrink-0 text-white hover:text-gray-700 border border-white hover:bg-white font-semibold rounded-md text-lg px-4 py-1 focus:outline-none" type="submit" v-on:click="adicionarMoeda" >Adicionar</button>
                 </div>
             </div>
         </div>
         <div class="sm:min-h-screen flex items-center p-3 lg:p-24 xl:p-48 h-screen justify-center items-center">
-            <div class="overscroll-auto overflow-y-scroll p-1  h-full ">
-                <div class="bg-white shadow-xl rounded-md border border-gray-300 p-2" v-for="(comment, index) in listaDeMoedas" v-bind:key="index">
+            <div class="overscroll-auto overflow-y-scroll p-1  h-full shadow-xl ">
+                <div class="bg-white rounded-md border border-gray-300 p-2" v-for="(comment, index) in listaDeMoedas" v-bind:key="index">
                     <div class="">
                         <div class="items-center flex text-center flex justify-content align-center">
                             <div class="flex-none text-xl">{{comment.name}}</div>
@@ -31,68 +31,59 @@
 </template>
 
 <script>
-let apikey = "9F6E1E03-4C3B-4854-8F1E-63E24538A0A0"
+let apikey = "71C52D18-5023-4D19-AB85-7287D7E54D02"
 export default{
    
     name: 'moedas',
     data() {
         return {
-                    moeda: [{ "name": "BTC", "valor": "" }, { "name": "SPG", "valor": "" }, { "name": "SPE", "valor": "" }, { "name": "WEMIX", "valor": "" },{ "name": "BCOIN", "valor": "" }],
+                    moeda: [{ "name": "BTC", "valor": "" }, { "name": "ETH", "valor": "" }, { "name": "BNB", "valor": "" }, { "name": "SHIB", "valor": "" },{ "name": "BCOIN", "valor": "" }],
                     name: '',
                     valor: "",
                     siglas: "",
                     nome_moeda: "",
                     verificar_erro : 0,
                     cadeira: "",
-                    backup : 0
+                    backup : 0,
+                    verificacao_lista:0
                 }
     
 }, methods: {
     mounted(element,operador) {
-        
-                   if (operador == 1){
-                       
-                             
-                             this.$axios.$get("https://rest.coinapi.io/v1/assets?filter_asset_id="+ element , { headers: {"X-CoinAPI-Key": apikey}})
-                            .then (response =>{
-                                
-                                this.cadeira = [response];
-                                this.atribuirValores()})
-                   } else{
-                       
-                             this.$axios.$get("https://rest.coinapi.io/v1/exchangerate/"+ element +"/USD" , { headers: {"X-CoinAPI-Key": apikey}})
-                            .then (response =>{
-                                this.cadeira = response,
-                                this.moeda.forEach(mylist => {
-                                    
-                                    if (mylist.__ob__.value.name === this.cadeira.__ob__.value.asset_id_base){
-                                    mylist.__ob__.value.valor = this.cadeira.__ob__.value.rate
-                                }
+        if (operador == 1){
+            this.$axios.$get("https://rest.coinapi.io/v1/assets?filter_asset_id="+ element , { headers: {"X-CoinAPI-Key": apikey}})
+            .then (response =>{
+                this.cadeira = [response];
+                this.atribuirValores()})
+                } else{
+                    this.$axios.$get("https://rest.coinapi.io/v1/exchangerate/"+ element +"/USD" , { headers: {"X-CoinAPI-Key": apikey}})
+                    .then (response =>{
+                        this.cadeira = response,
+                        this.moeda.forEach(mylist => {
+                            if (mylist.__ob__.value.name === this.cadeira.__ob__.value.asset_id_base){
+                                mylist.__ob__.value.valor = this.cadeira.__ob__.value.rate}
                                 });
-                                this.atualizarMoedas()
+                            this.atualizarMoedas()
                                 })
-                            .catch(error =>{ this.verificar_erro= error
-                            this.moeda.splice(-1,1),
-                            alert("Esta moeda não foi encontrada!")}
-                            )
-
-                   }
-                
-                },
-                atribuirValores(){
+                    .catch(error =>{ this.verificar_erro= error
                     
-                       this.cadeira[0].forEach(api => {
-                            this.moeda.forEach(mylist => { 
-                                if (mylist.__ob__.value.name === api.asset_id){
-                                    
-                                    this.backup =  mylist.__ob__.value.valor 
-                                    mylist.__ob__.value.valor = api.price_usd
-                                    if ( mylist.__ob__.value.name === 'USD'){
-                                        //console.log("to caindo na condição")
-                                         mylist.__ob__.value.valor = this.backup
-                                         this.backup = 0
-                                    }
-                                }
+                    this.moeda.splice(-1,1),
+                    alert("Esta moeda não foi encontrada!")}
+                            )
+                   }
+                },
+    atribuirValores(){
+        this.cadeira[0].forEach(api => {
+            this.moeda.forEach(mylist => {
+                if (mylist.__ob__.value.name === api.asset_id){
+                    this.backup =  mylist.__ob__.value.valor
+                    mylist.__ob__.value.valor = api.price_usd
+                    if ( mylist.__ob__.value.name === 'USD'){
+                        //console.log("to caindo na condição")
+                        mylist.__ob__.value.valor = this.backup
+                        this.backup = 0
+                        }
+                         }
                             });
                         });
 
@@ -105,18 +96,34 @@ export default{
                     this.mounted(this.nome_moeda,1)
                 },
      adicionarMoeda(){
-                    if ( this.name.trim()===''){
-                        return;
+        if ( this.name.trim()===''){
+            return;
+        }
+        this.name = this.name.toUpperCase()
+        if (this.verificacao_lista === 0){
+            this.moeda.forEach(element => {
+                if (this.name===element.name){
+                    this.verificacao_lista = 1
+                }
+            });
+        }
+        if (this.verificacao_lista === 1){
+            alert("Esta cryptomoeda já está na lista!")
+            this.verificacao_lista = 0,
+            this.name = ""
+            return
+        } else {
+            this.moeda.push({
+            name: this.name,
+            valor: this.valor
+            });
+            this.mounted(this.name,0)
+            this.name ="",
+            this.valor= ""
                     }
-                    this.name = this.name.toUpperCase()
                     
-                    this.moeda.push({
-                        name: this.name,
-                        valor: this.valor
-                    });
-                    this.mounted(this.name,0)
-                    this.name ="",
-                    this.valor= ""
+                        
+                   
      },removerMoeda(index){
         this.moeda.splice(index,1);
      },
@@ -125,7 +132,7 @@ export default{
                         this.verificar_erro = this.verificar_erro + 1,
                         this.atualizarMoedas()
                     }else{
-                        setTimeout(() => { this.verificar_erro= 0,
+                        setTimeout(() => { this.atualizarMoedas(),
                         console.log("bateu 30") } , 30000)
                     }
                 }
@@ -135,8 +142,8 @@ export default{
     computed: {
         listaDeMoedas(){
  return this.moeda.map(comment => ({
-                        ...comment,
-                        name: comment.name.trim() ==='' ? 'Anonimo' : comment.name
+                        ...comment
+                        
                     }))
         }
     }
